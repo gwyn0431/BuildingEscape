97e0bc72ee6d73ff42e58972a23abac1e6cb065f
gwyn0431 <gwyn0431@gmail.com>
2018-02-10 00:19:27 +0300

    Added Chair and Table to source control.

--- BuildingEscape/Source/BuildingEscape/PositionReport.h
+++ BuildingEscape/Source/BuildingEscape/PositionReport.h
@@ -22,9 +22,6 @@ protected:
 
 public:	
 	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, 
-		                       FActorComponentTickFunction* ThisTickFunction) override;
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
-	
 };
